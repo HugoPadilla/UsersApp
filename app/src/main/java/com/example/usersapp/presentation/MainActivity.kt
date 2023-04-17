ce04e06b8e397915028266b87493172afe9ebba3
Hugo Padilla <hugojavier19890@gmail.com>
2023-04-16 22:53:22 -0500

    feat: add search functionality in the top bar

--- app/src/main/java/com/example/usersapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/usersapp/presentation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.usersapp.presentation
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.usersapp.R
 import com.example.usersapp.databinding.ActivityMainBinding
 import com.example.usersapp.presentation.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        val adapter = UserAdapter()
+        adapter = UserAdapter()
         val linearlayout = LinearLayoutManager(this)
         binding.recycler.setHasFixedSize(true)
         binding.recycler.layoutManager = linearlayout
@@ -30,5 +34,26 @@ class MainActivity : AppCompatActivity() {
             adapter.setData(it)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val menuItem = menu.findItem(R.id.app_bar_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.queryHint = "Search by name"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter(newText)
+                return false
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/example/usersapp/presentation/UserAdapter.kt
+++ app/src/main/java/com/example/usersapp/presentation/UserAdapter.kt
@@ -1,18 +1,65 @@
 package com.example.usersapp.presentation
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.usersapp.databinding.ItemUserBinding
 import com.example.usersapp.domain.models.User
+import java.util.*
+import java.util.stream.Collectors
 
 class UserAdapter : RecyclerView.Adapter<UserAdapter.Holder>() {
 
+    private var listUser: ArrayList<User> = ArrayList()
     private var listUserOrigin: ArrayList<User> = ArrayList()
 
     fun setData(listUser: List<User>) {
+        this.listUser.clear()
+        this.listUser.addAll(listUser)
         listUserOrigin.clear()
         listUserOrigin.addAll(listUser)
+        notifyDataSetChanged()
+    }
+
+    /**
+     * Filters the list of users [listUser] based on the provided [queryText].
+     * If [queryText] is an empty string, the list [listUser] is restored to its original state [listUserOrigin].
+     * Otherwise, the list [listUser] is searched for all [User] objects whose name contains [queryText] (ignoring case).
+     * The result is saved in a new list named "collection".
+     *
+     * If the SDK version is lower than N, the search is performed through a "for" loop over the [listUserOrigin] list and matching elements are added to the [listUser] list.
+     * Otherwise, the search is performed using the Stream API.
+     *
+     * The [notifyDataSetChanged()] method is called after updating the [listUser] list to reflect the changes in the corresponding view.
+     *
+     * @param queryText Text string used to filter the list of users.
+     */
+    fun filter(queryText: String) {
+        val lengthText = queryText.length
+        if (lengthText == 0) {
+            listUser.clear()
+            listUser.addAll(listUserOrigin)
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                val collection: List<User> = listUser.stream()
+                    .filter { user ->
+                        user.name.lowercase()
+                            .contains(queryText.lowercase(Locale.getDefault()))
+                    }
+                    .collect(Collectors.toList())
+                listUser.clear()
+                listUser.addAll(collection)
+            } else {
+                for (user in listUserOrigin) {
+                    if (user.name.lowercase()
+                            .contains(queryText.lowercase(Locale.getDefault()))
+                    ) {
+                        listUser.add(user)
+                    }
+                }
+            }
+        }
         notifyDataSetChanged()
     }
 
@@ -33,11 +80,11 @@ class UserAdapter : RecyclerView.Adapter<UserAdapter.Holder>() {
     }
 
     override fun getItemCount(): Int {
-        return listUserOrigin.size
+        return listUser.size
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        val item = listUserOrigin[position]
+        val item = listUser[position]
         holder.bind(item)
     }
 
